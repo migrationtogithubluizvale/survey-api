265cbffef596d3fb8b888c94b4f0ae208b1f416c
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2021-10-24 09:12:13 -0300

    feat: add survey models for answers and results

---
+++ src/domain/models/survey-result.ts
@@ -0,0 +1,14 @@
+export interface SurveyResultModel {
+  surveyId: string
+  question: string
+  answers: SurveyResultAnswerModel[]
+  date: Date
+}
+
+interface SurveyResultAnswerModel {
+  image?: string
+  answer: string
+  count: number
+  percent: number
+  isCurrentAccountAnswer: boolean
+}
