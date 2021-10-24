48254c6424010b1dbad331d3f2991c0515aec218
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2021-10-24 11:20:26 -0300

    feat: implement http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,17 +1,32 @@
-import { HttpResponse } from '../protocols/http'
-import { ServerError } from '../errors'
+import { HttpResponse } from '@/presentation/protocols'
+import { ServerError, UnauthorizedError } from '@/presentation/errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
 
-export const serverError = (): HttpResponse => ({
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
-  body: new ServerError()
+  body: new ServerError(error.stack)
 })
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
 })
